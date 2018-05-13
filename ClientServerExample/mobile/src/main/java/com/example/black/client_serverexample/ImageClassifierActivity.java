f443860090f89baba991026bbcdfae5e2d26b856
neo96 <blackperry@hotmail.it>
2018-05-13 10:13:11 +0200

    join

--- ClientServerExample/mobile/src/main/java/com/example/black/client_serverexample/ImageClassifierActivity.java
+++ ClientServerExample/mobile/src/main/java/com/example/black/client_serverexample/ImageClassifierActivity.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
+
 public class ImageClassifierActivity extends Activity {
     private static final String TAG = "ImageClassifierActivity";
 
